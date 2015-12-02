c5f7e6b342a521ddbcb2bde11f2963e5d0ffbf9e
xchen89 <xchen89@buffalo.edu>
2015-12-02 15:57:22 -0500

    crossword game

--- src/Control/controls.java
+++ src/Control/controls.java
@@ -13,28 +13,85 @@ public class controls {
 		WordList curWord = list;
 		//create and add the first word
 		Position firstPosition = rdmPosition(null);
-		String res = getStringRestrictions(firstPosition, 0);
-		String w = randomWord(res);
+		String res = getStringRestrictions(gb, firstPosition, 0);
+		String w = randomWord(res, 7);
 		curWord.next = new WordList(w, firstPosition, 0);
 		curWord = curWord.next;
-		while (gb.isFull()) {
+		addWord(gb, w, firstPosition, curWord.getDirection());
+		
+		//TODO implement isFull and use while loop to generate wordlist.
+		/*
+		//while (gb.isFull()) {
 			//TODO need to deal with backtracking cases
 			Position nextPosition = rdmPosition(curWord);
-			String restrictions = getStringRestrictions(nextPosition, curWord.getDirection());
-			String word = randomWord(restrictions);
-			curWord.next = new WordList(word, nextPosition, (curWord.getDirection()^1));
+			if (nextPosition!=null) {
+				String restrictions = getStringRestrictions(gb, nextPosition, (curWord.getDirection()^1));
+				int len = 0;
+				//last is horizontal, next will be vertical. then use x to cal
+				if (curWord.getDirection()==0) len = rdmlen(x-nextPosition.getX());
+				else len = rdmlen(x-nextPosition.getY());
+				String word = randomWord(restrictions, len);
+				curWord.next = new WordList(word, nextPosition, (curWord.getDirection()^1));
+				curWord = curWord.next;
+			}
+			else {
+				//reach the bottom of the board. need to start from top again to squzze in more words
+			}
+		//}
+		*/
+			//TODO all numbers should be random
+			Position nextPosition = new Position(0,3);//rdmPosition needs to be done
+			String word = randomWord(" ", 6);		  //ramdomWord needs to be done
+			curWord.next = new WordList(word, nextPosition, curWord.getDirection()^1);
 			curWord = curWord.next;
-		}
-		
+			addWord(gb, word, nextPosition, curWord.getDirection());
+			
+			nextPosition = new Position(2, 3);
+			word = randomWord("N      ", 4);
+			curWord.next = new WordList(word, nextPosition, curWord.getDirection()^1);
+			curWord = curWord.next;
+			addWord(gb, word, nextPosition, curWord.getDirection());
+			
+			nextPosition = new Position(2, 6);
+			word = randomWord("T     ", 5);
+			curWord.next = new WordList(word, nextPosition, curWord.getDirection()^1);
+			curWord = curWord.next;
+			addWord(gb, word, nextPosition, curWord.getDirection());
+			
+			nextPosition = new Position(4,3);
+			word = randomWord("  A ", 4);
+			Position tmp = new Position(nextPosition.getX(), nextPosition.getY()-2);
+			curWord.next = new WordList(word, tmp, curWord.getDirection()^1);
+			curWord = curWord.next;
+			addWord(gb, word, tmp, curWord.getDirection());
+			
+			nextPosition = new Position(4,1);
+			word = "FOOL";
+			curWord.next = new WordList(word, nextPosition, curWord.getDirection()^1);
+			curWord = curWord.next;
+			addWord(gb, word, nextPosition, curWord.getDirection());
+			
+			nextPosition = new Position(6,6);
+			word = randomWord("CONTRACT", 9);
+			tmp = new Position(nextPosition.getX(), nextPosition.getY()-6);
+			curWord.next = new WordList(word, tmp, curWord.getDirection()^1);
+			curWord = curWord.next;
+			addWord(gb, word, tmp, curWord.getDirection());
+			
 		//print out the word list for now.
 		//TODO UI design based on the list
-		printLayout(list);
+		printLayout(gb);
 	}
 	
-	//add length as a restriction?
-	//public String randomWord(String restrictions, int length)
-	public String randomWord(String restrictions) {
+	public String randomWord(String restrictions, int length) {
 		//search the dictionary and return the first word that meets the criteria
+		if (length==7) return "STUDENT";
+		else if (length==9) return "CONTRACTS";
+		else if (length==6) return "DENTAL";
+		else if (length==5) return "TOXIC";
+		
+		else if (length==4 && restrictions == "N      ") return "NEXT";
+		else if (length==4) return "FLAG";
 		return null;
 	}
 
@@ -42,31 +99,66 @@ public class controls {
 		if (word == null) return new Position(0, 0);
 		int x=0, y=0;
 		Random rm = new Random();
-		
-		if (word.getDirection()==0) {
+		//vertical
+		if (word.getDirection()==1) {
 			y = word.getPosition().getY();
 			x = rm.nextInt(word.getWord().length())-word.getPosition().getX();
 			return new Position(x, y);
 		}
+		//horizontal
 		else {
 			x = word.getPosition().getX();
 			y = rm.nextInt(word.getWord().length())-word.getPosition().getY();
-			return new Position(x,y);
+			return new Position(x, y);
 		}
 	}
-	/*
+	
 	public int rdmlen(int max) {
 		Random rm = new Random();
 		return rm.nextInt(max);
 	}
-	*/
-	public String getStringRestrictions(Position p, int direction) {
+	
+	public String getStringRestrictions(GameBoard gb, Position p, int direction) {
 		//return a string to indicate any existing chars.
 		//"s  t  "
-		return "";
+		StringBuffer ans = new StringBuffer();
+		if (direction == 0) {
+			//horizontal
+			char[] row = gb.getRow(p.getX());
+			for (int i=p.getY();i<row.length;i++) ans.append(row[i]);
+		}
+		else {
+			//vertical
+			char[] col = gb.getCol(p.getY());
+			for (int i=p.getX();i<col.length;i++) ans.append(col[i]);
+		}
+		return ans.toString();
 	}
 	
-	public void printLayout(WordList list) {
+	public void addWord(GameBoard gb, String word, Position p, int d) {
+		int row = p.getX(), col = p.getY();
+		if (d==1) {
+			for (int i=0;i<word.length();i++) {
+				gb.setEntry(row, col, word.charAt(i));
+				row++;
+			}
+		}
+		else {
+			for (int i=0;i<word.length();i++) {
+				gb.setEntry(row, col, word.charAt(i));
+				col++;
+			}
+		}
+	}
+	
+	public void printLayout(GameBoard gb) {
 		//to return the result
+		
+		for (int i=0;i<gb.size();i++) {
+			for (int j=0;j<gb.size();j++) {
+				System.out.print("|"+gb.getEntry(i,j));
+			}
+			System.out.print("\n");
+		}
 	}
 }
--- src/Model/GameBoard.java
+++ src/Model/GameBoard.java
@@ -4,9 +4,38 @@ public class GameBoard {
 	char[][] board;
 	public GameBoard(int x) {
 		board = new char[x][x];
+		for (int i=0;i<x;i++) {
+			for (int j=0;j<x;j++) {
+				board[i][i] = 32;
+			}
+		}
 	}
 	
 	public boolean isFull() {
-		return true;
+		return false;
+	}
+	
+	public char[] getRow(int row) {
+		return board[row];
+	}
+	
+	public char[] getCol(int col) {
+		char[] ans = new char[board.length];
+		for (int i=0;i<board.length;i++) {
+			ans[i]=board[i][col];
+		}
+		return ans;
+	}
+	
+	public void setEntry(int x, int y, char c) {
+		board[x][y] = c;
+	}
+	
+	public char getEntry(int x, int y) {
+		return board[x][y];
+	}
+	
+	public int size() {
+		return board.length;
 	}
 }
